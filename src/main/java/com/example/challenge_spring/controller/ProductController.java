fc2f6be5b275efc2af629a480e24123a19aee234
franklinlopes <gabriel.franklin@mercadolivre.com>
2022-10-05 17:35:06 -0300

    feat(product): criada busca de todos os produtos

--- src/main/java/com/example/challenge_spring/controller/ProductController.java
+++ src/main/java/com/example/challenge_spring/controller/ProductController.java
@@ -6,12 +6,10 @@ import com.example.challenge_spring.service.IProduct;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
+import java.util.List;
 
 
 @RestController
@@ -24,5 +22,10 @@ public class ProductController {
     public ResponseEntity<ProductDto> createProduct(@RequestBody Product product) throws IOException {
         System.out.println(product);
         return ResponseEntity.status(HttpStatus.CREATED).body(service.createProduct(product));
+    }
+
+    @GetMapping("/articles")
+    public ResponseEntity<List<Product>> getAll(){
+        return ResponseEntity.status(HttpStatus.OK).body(service.getAll());
     }
 }
--- src/main/java/com/example/challenge_spring/service/IProduct.java
+++ src/main/java/com/example/challenge_spring/service/IProduct.java
@@ -4,7 +4,10 @@ import com.example.challenge_spring.dto.ProductDto;
 import com.example.challenge_spring.model.Product;
 
 import java.io.IOException;
+import java.util.List;
 
 public interface IProduct {
     ProductDto createProduct(Product product) throws IOException;
+
+    List<Product> getAll();
 }
