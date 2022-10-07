cd8c37c2e0e258fd51ae38616d10e9c1b8acff12
franklinlopes <gabriel.franklin@mercadolivre.com>
2022-10-07 15:00:58 -0300

    docs(ProductOrder): add comments

--- src/main/java/com/example/challenge_spring/util/ProductOrder.java
+++ src/main/java/com/example/challenge_spring/util/ProductOrder.java
@@ -2,13 +2,14 @@ package com.example.challenge_spring.util;
 
 import com.example.challenge_spring.model.Product;
 
+// Classe utilitária para aplicar possíveis ordenações ao resultado das buscas.
 public class ProductOrder {
 
     public static int orderProducts(int order, Product p1, Product p2) {
         if (order == 0) return alphabeticalOrder(p1, p2);
-        if (order == 1) return alphabeticalOrder(p2, p1);
+        if (order == 1) return alphabeticalOrder(p2, p1); // Ordenação inversa
         if (order == 2) return valueOrder(p2, p1);
-        if (order == 3) return valueOrder(p1, p2);
+        if (order == 3) return valueOrder(p1, p2); // Ordenação inversa
         return 0;
     }
 
